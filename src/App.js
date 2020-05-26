79b9928a2a16ecce9e5b41977f4567b8e9f30a57
Sanket Manmode <manmodesanket@gmail.com>
2020-05-26 10:50:58 +0530

    router and basic components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import About from "./components/about";
+import Intro from "./components/intro";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
 import "./css/styles.css";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <div className="content">
+        <Navbar />
+        <Router>
+          <Intro path="/" />
+          <About path="/about" />
+          <Projects path="/projects" />
+        </Router>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
