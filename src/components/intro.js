4903fd52dca96263c5482daefe62e66347b5fb02
Sanket Manmode <manmodesanket@gmail.com>
2020-05-30 18:21:43 +0530

    intro changed

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,14 +5,14 @@ const Intro = () => {
   return (
     <div>
       <div className="intro">
-        <h1>Hello 👋</h1>
-        <h2 className="intro_para">
-          I'm <span>Sanket Manmode</span>, I call myself a Software Engineer. I
-          am a Final year Engineering Student at MIT College of Engineering,
-          Pune majoring in IT branch. Currently, I am in love with React and
-          learning new things everyday.
-        </h2>
-        <div className="email">Get in touch at: manmodesanket@gmail.com</div>
+        <div className="intro"></div>
+        <div className="div name">Hello 👋 I am,</div>
+        <div className="div name">
+          <span>Sanket Manmode</span>
+        </div>
+        <div className="div prof">Software Engineer</div>
+        <div className="div">Passionate about JavaScript</div>
+        <div className="div">Learning new things everyday.</div>
       </div>
     </div>
   );
